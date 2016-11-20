0c236c25a5d528e9d1299a150d6bb59005ad3f78
Shilpa Goel <shilpa92@ufl.edu>
2016-11-20 16:18:05 -0500

    added SVM

--- scalaSpark.scala
+++ scalaSpark.scala
@@ -2,8 +2,8 @@ import org.apache.spark._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.mllib.tree.DecisionTree
-import org.apache.spark.mllib.tree.model.DecisionTreeModel
+import org.apache.spark.mllib.classification.SVMWithSGD
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
 
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
@@ -38,7 +38,8 @@ object SimpleApp {
 	}
 
 	def main(args: Array[String]) {
-		val conf = new SparkConf().setAppName("Simple Application for testing").setMaster("local[*]")
+		val conf = new SparkConf().setAppName("Simple Application for testing")
+		//setMaster("local[*]").set("spark.executor.memory", "4g")
     	val sc = new SparkContext(conf)
 		var textRDD = sc.textFile(args(0))
 		val header = textRDD.first()
@@ -89,10 +90,77 @@ object SimpleApp {
 			})
 		println(mlprep.take(1).deep.mkString("\n"))
 
+		// 0 == not delayed , 1 == delayed
 		val mldata = mlprep.map(
 			x => LabeledPoint(x(0), Vectors.dense(x(1), x(2), x(3), x(4), x(5), x(6), x(7), x(8))
 			))
 		println(mldata.take(1).deep.mkString("\n"))
+		
+		val mldata0 = mldata.filter(x => x.label == 0).randomSplit(Array(0.85, 0.15))(1)				// mldata0 is %85 not delayed flights
+		val mldata1 = mldata.filter(x => x.label != 0)							// mldata1 is %100 delayed flights
+		val mldata2 = mldata0 ++ mldata1										// mldata2 is delayed and not delayed
+
+		val splits = mldata2.randomSplit(Array(0.8, 0.2))
+		val trainingRDD = splits(0).cache()
+		val testingRDD = splits(1).cache()
+
+		println(testingRDD.take(1).deep.mkString("\n"))
+
+		// Build the SVM model
+		val svmAlg = new SVMWithSGD()
+		svmAlg.optimizer.setNumIterations(100)
+		                .setRegParam(1.0)
+		                .setStepSize(1.0)
+		val model_svm = svmAlg.run(trainingRDD)
+
+		// Predict
+		val labelsAndScores = testingRDD.map { point =>
+		        val pred = model_svm.predict(point.features)
+		        ( point.label, pred)
+		}
+		val metrics = new BinaryClassificationMetrics(labelsAndScores)
+	
+
+		// Precision by threshold
+		val precision = metrics.precisionByThreshold
+		precision.foreach { case (t, p) =>
+		  println(s"Threshold: $t, Precision: $p")
+		}
+
+		// Recall by threshold
+		val recall = metrics.recallByThreshold
+		recall.foreach { case (t, r) =>
+		  println(s"Threshold: $t, Recall: $r")
+		}
+
+		// Precision-Recall Curve
+		val PRC = metrics.pr
+
+		// F-measure
+		val f1Score = metrics.fMeasureByThreshold
+		f1Score.foreach { case (t, f) =>
+		  println(s"Threshold: $t, F-score: $f, Beta = 1")
+		}
+
+		val beta = 0.5
+		val fScore = metrics.fMeasureByThreshold(beta)
+		f1Score.foreach { case (t, f) =>
+		  println(s"Threshold: $t, F-score: $f, Beta = 0.5")
+		}
+
+		// AUPRC
+		val auPRC = metrics.areaUnderPR
+		println("Area under precision-recall curve = " + auPRC)
+
+		// Compute thresholds used in ROC and PR curves
+		val thresholds = precision.map(_._1)
+
+		// ROC Curve
+		val roc = metrics.roc
+
+		// AUROC
+		val auROC = metrics.areaUnderROC
+		println("Area under ROC = " + auROC)
 
 		sc.stop
 	}
